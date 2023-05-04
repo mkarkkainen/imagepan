e215936a96dd2aca4b1cd9a7d90fcd6a151af9e6
Miikka Kärkkäinen <miikkak@tuta.io>
2023-05-03 23:50:08 -0600

    second commit..

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const dots = document.querySelectorAll('.dot')
 const panner = document.getElementById('left')
 
 dots.forEach((dot) => {
-	dot.addEventListener('click', () => {
-		dots.forEach((dot) => dot.classList.remove('animation'))
-		panner.classList.remove('animation')
+	dot.addEventListener('click', (e) => {
+		console.log(e)
 	})
 })
